85ae7b508fdfd04782d05bdc2a2938ba80533fd8
Diogo Freitas <diogo.freitas@hotmail.com>
2021-05-01 19:03:04 -0300

    Implementando Update e Delete

--- Controllers/InfectadoController.cs
+++ Controllers/InfectadoController.cs
@@ -2,6 +2,7 @@
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using System;
 
 namespace Api.Controllers
 {
@@ -21,7 +22,7 @@ namespace Api.Controllers
         [HttpPost]
         public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
         {
-            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude, null);
 
             _infectadosCollection.InsertOne(infectado);
             
@@ -34,6 +35,28 @@ namespace Api.Controllers
             var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
             
             return Ok(infectados);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult AtualizarInfectado(string id, [FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dataNascimento: dto.DataNascimento, sexo: dto.Sexo,
+                latitude: dto.Latitude, longitude: dto.Longitude, id: id);
+
+            var filter = Builders<Infectado>.Filter.Eq(s => s.Id, id);
+            var result = _infectadosCollection.ReplaceOne(filter, infectado);
+
+            //_infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(c => c.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("Sexo", dto.Sexo));
+
+            return Ok("Atualizado com sucesso");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeletarInfectado(string id)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(c => c.Id == id));
+
+            return Ok("Deletado com sucesso");
         }
     }
 }
--- Data/Collections/Infectado.cs
+++ Data/Collections/Infectado.cs
@@ -1,17 +1,22 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver.GeoJsonObjectModel;
 
 namespace Api.Data.Collections
 {
     public class Infectado
     {
-        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude, string id)
         {
             this.DataNascimento = dataNascimento;
             this.Sexo = sexo;
             this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+            this.Id = id;
         }
-        
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public String Id { get; set; }
         public DateTime DataNascimento { get; set; }
         public string Sexo { get; set; }
         public GeoJson2DGeographicCoordinates Localizacao { get; set; }
